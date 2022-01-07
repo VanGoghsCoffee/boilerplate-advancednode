d18ef972a92b8b8be4a6b25fb8b78c6706af6cca
David Rochholz <mail@davidrochholz.de>
2022-01-08 00:28:48 +0100

    Authentication Strategies

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const ObjectID = require('mongodb').ObjectID;
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -44,6 +45,18 @@ myDB(async client => {
       done(null, doc);
     });
   });
+
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function(err, user) {
+        console.log('user ' + username + ' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password ) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
 
 }).catch(e => {
   app.route('/').get((req, res) => {
