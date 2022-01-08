208bd37ff2263d0aaebe9c1de302953239833153
David Rochholz <mail@davidrochholz.de>
2022-01-08 17:43:49 +0100

    How to Use Passport Strategies

--- server.js
+++ server.js
@@ -1,68 +1,13 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection');
-const ObjectID = require('mongodb').ObjectID;
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const myApp = require('./app');
 
 const app = express();
-
-app.set('view engine', 'pug');
+app.use(myApp);
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'HOLA',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-myDB(async client => {
-
-  const myDataBase = await client.db('database').collection('users');
-
-  app.route('/').get((req, res) => {
-    res.render('pug/index', { title: 'Connected to Database', message: 'Please login' });
-  });
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-  
-  passport.deserializeUser((id, done) => {
-    myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  passport.use(new LocalStrategy(
-    function(username, password, done) {
-      myDataBase.findOne({ username: username }, function(err, user) {
-        console.log('user ' + username + ' attempted to log in.');
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (password !== user.password ) { return done(null, false); }
-        return done(null, user);
-      });
-    }
-  ));
-
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('pug/index', { title: e, message: 'Unable to login' });
-  });
-});
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
