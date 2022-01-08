d9dbcb344832ebf0527ea1a0929441a1c4278a6e
David Rochholz <mail@davidrochholz.de>
2022-01-08 17:54:06 +0100

    How to Put a Profile Together

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ myDB(async client => {
 
   app.route('/profile')
     .get(ensureAuthenticated, (req, res) => {
-      res.render('pug/profile');
+      res.render('pug/profile', { username: req.user.username });
     });
 
   app.route('/login')
