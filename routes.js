81d1011aed66f680ac99bba62167ec527015a53b
David Rochholz <mail@davidrochholz.de>
2022-01-09 01:31:45 +0100

    Implementation of Social Authentication

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, myDataBase) {
 
     app.route('/')
     .get((req, res) => {
-        res.render('pug/index', { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true });
+        res.render('pug/index', { title: 'Connected to Database', message: 'Please login', showLogin: true, showRegistration: true, showSocialAuth: true });
     });
     app.route('/profile')
         .get(ensureAuthenticated, (req, res) => {
@@ -46,6 +46,14 @@ module.exports = function(app, myDataBase) {
         .get((req, res) => {
         req.logout();
         res.redirect('/');
+        });
+
+    app.route('/auth/github')
+        .get(passport.authenticate('github'));
+
+    app.route('/auth/github/callback')
+        .get(passport.authenticate('github', { failureRedirect: '/'}), (req, res) => {
+            res.redirect('/profile');
         });
 
     app.use((req, res, next) => {
