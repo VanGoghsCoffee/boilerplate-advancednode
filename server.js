e92610a7665cdc19b782e5184432e8bf4c08a5b8
David Rochholz <mail@davidrochholz.de>
2022-01-22 15:46:50 +0100

    Authentication with Socket.IO

--- server.js
+++ server.js
@@ -8,19 +8,36 @@ const auth = require('./auth');
 const session = require('express-session');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const cors = require('cors');
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
 
 const app = express();
 app.use(cors());
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
+  key: 'express.sid',
   secret: process.env.SESSION_SECRET || 'HOLA',
   resave: true,
+  store: store,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
@@ -37,7 +54,7 @@ myDB(async client => {
   let currentUsers = 0;
   io.on('connection', socket => {
     ++currentUsers;
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.name + ' connected');
     io.emit('user count', currentUsers);
 
     socket.on('disconnect', () => {
@@ -51,6 +68,19 @@ myDB(async client => {
     res.render('pug/index', { title: e, message: 'Unable to login' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 fccTesting(app); //For FCC testing purposes
 
