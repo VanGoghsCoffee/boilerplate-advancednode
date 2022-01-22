1b2c1fede7bf7951dea9dd7baf3291787cb8407d
David Rochholz <mail@davidrochholz.de>
2022-01-22 14:13:00 +0100

    Set up the Environment

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const session = require('express-session');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -28,6 +32,9 @@ myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   routes(app, myDataBase);
   auth(app, myDataBase);
+  io.on('connection', socket => {
+    console.log('A user has connected');
+  })
 
 }).catch(e => {
   app.route('/').get((req, res) => {
@@ -38,6 +45,6 @@ myDB(async client => {
 fccTesting(app); //For FCC testing purposes
 
 const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
